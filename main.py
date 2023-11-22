c1f5d26fa1c289060261158a7e86129b649ad089
Pedro Abreu <abreu223@hotmail.com>
2023-11-22 15:29:19 -0500

    Cleanup main

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ import Utils
 import Train
 
 def main():
-    for arg in sys.argv:
-        print(arg)
-
     opt = Utils.arg_parser_subst(sys.argv)
-
     Train.train(opt)
-
-    return
 
 
 if __name__ == "__main__":
